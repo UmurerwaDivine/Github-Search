0cc53512f49cbd5f01b73bf4b5f2c6c8e9e9a969
UmurerwaDivine <divinekagame@gmail.com>
2019-02-05 11:39:28 +0200

    display files

--- src/app/search.ts
+++ src/app/search.ts
@@ -1,6 +1,6 @@
 export class Search {
-    constructor(public gravatar_id:string,public name:string,public location:string,public public_repos:number,public followers:number,public following:number,public repos_url:string){
-        
+    constructor(public avatar_url:string,public name:string,public location:string,public public_repos:number,public followers:number,public following:number,public html_url:string){
+
     }
 }
   
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -15,16 +15,19 @@ this.search=new Search("","","",0,0,0,"")
 }
   ngOnInit() {
     interface ApiResponse{
-      gravatar_id:string;
+      avatar_url:string;
       name:string;
       location:string;
       public_repos:number;
       followers:number;
        following:number;
-       repos_url:string
+       html_url:string
   }
   this.http.get<ApiResponse>("https://api.github.com/users/daneden?access_token"+environment.apiKey).subscribe(data=>{
-      this.search= new Search(data.gravatar_id,data.name,data.location,data.public_repos,data.followers,data.following,data.repos_url);
+      this.search= new Search(data.avatar_url,data.name,data.location,data.public_repos,data.followers,data.following,data.html_url);
+    },err=>{
+      this.search= new Search("Never", "never", "never", 0,0,0, "winston churchill")
+      console.log("Error occured ")
   })
 }
 
